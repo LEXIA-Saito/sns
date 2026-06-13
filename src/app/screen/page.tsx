23de59d7534d23ba28f1bd1eb48fc9e61b24d489
Saito <95484621+LEXIA-Saito@users.noreply.github.com>
2026-06-13 22:14:41 +0900

    Remove screen display page

--- src/app/screen/page.tsx
+++
@@ -1,9 +0,0 @@
-import ScreenView from "@/components/ScreenView";
-
-export const metadata = {
-  title: "26アカデミー | スクリーン表示",
-};
-
-export default function ScreenPage() {
-  return <ScreenView />;
-}
